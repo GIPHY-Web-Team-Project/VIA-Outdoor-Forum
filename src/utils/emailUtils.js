12d31a9bdb7e01d88617d74f77bb4b58a83a8145
Alexandra Zhivkova <alexandrazhivkova9@gmail.com>
2025-02-15 13:35:21 +0200

    Added registration form, connected to the database.

---
+++ src/utils/emailUtils.js
@@ -0,0 +1,7 @@
+export const encodeEmail = (email) => {
+    return email.replace(/\./g, ',');
+  };
+  
+export const decodeEmail = (encodedEmail) => {
+    return encodedEmail.replace(/,/g, '.');
+};
