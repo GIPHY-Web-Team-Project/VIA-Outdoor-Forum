f195e0748f05faeeffc8abbb51140de48b963102
Viktor Kolev <118537287+smithy773@users.noreply.github.com>
2025-02-15 11:39:19 +0200

    Update firebase-config.js
    Added the correct firebase database config to the firebase config file, since we updated the database to Google account belonging to the team.

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -3,15 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBp9nwJvAKyLer4xPeNCKr2J4rIDbblXg8",
-    authDomain: "via-outdoors.firebaseapp.com",
-    projectId: "via-outdoors",
-    storageBucket: "via-outdoors.firebasestorage.app",
-    messagingSenderId: "182572807658",
-    appId: "1:182572807658:web:4a7f66b019f4e43035ee93",
-    databaseUrl: "https://via-outdoors-default-rtdb.europe-west1.firebasedatabase.app/",
-  };
-
+  apiKey: "AIzaSyAmrE0nAYy5jlREDk4QHm_kbmQVaQBZrlc",
+  authDomain: "via-outdoors-d6d97.firebaseapp.com",
+  databaseURL: "https://via-outdoors-d6d97-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "via-outdoors-d6d97",
+  storageBucket: "via-outdoors-d6d97.firebasestorage.app",
+  messagingSenderId: "943670918695",
+  appId: "1:943670918695:web:2e589a125a49bc71891802",
+  databaseUrl: "https://via-outdoors-d6d97-default-rtdb.europe-west1.firebasedatabase.app/",
+};
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
